91d0edb35a062fd26d5e809752247b71bc6fba3e
anthax589 <jaymartuba2@gmail.com>
2024-12-22 20:53:32 +0800

    add hover effects on cards

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -29,11 +29,11 @@ function Home() {
                 onClick={toggleNavbar}
                 data-collapse-toggle="navbar-default"
                 type="button"
-                className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm  rounded-lg md:hidden hover:bg-gray-100 focus:outline-none  dark:text-gray-400  "
+                className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm  rounded-lg md:hidden  dark:text-gray-400  "
                 aria-controls="navbar-default"
                 aria-expanded="false"
               >
-                <span className="sr-only">Open main menu</span>
+                <span className="sr-only">menu</span>
                 <svg
                   className="w-5 h-5 text-black"
                   aria-hidden="true"
@@ -54,7 +54,7 @@ function Home() {
                 className="hidden w-full md:block md:w-auto"
                 id="navbar-default"
               >
-                <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white items-center">
+                <ul className="font-medium flex flex-col p-4 md:p-0 mt-4  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white items-center">
                   <li>
                     <a
                       href="#home"
@@ -94,10 +94,12 @@ function Home() {
           <div className="flex flex-col md:flex-row items-center justify-center gap-20 min-h-screen">
             <div className="md:p-5 md:items-center md:justify-center">
               <h1 className="text-2xl font-medium text-black md:text-4xl">
-                Welcome to{" "}
-                <span className="text-green-500 md:text-3xl">AnThraX</span>
+                Hi! everyone,{" "}
               </h1>
-              <p className="text-md text-gray-800 mb-4 text-wrap py-2 md:text-2xl">
+              <h1 className=" text-2xl font-medium text-black md:text-4xl">
+                I'm<span className="text-4xl md:text-6xl"> Jaymar Tuba</span>
+              </h1>
+              <p className="text-2xl font-medium text-gray-800 mb-4 text-wrap py-2 md:text-4xl">
                 Web <span className="text-green-500">Developer</span>
               </p>
               <button className="bg-black text-white text-sm py-2 px-4 rounded cursor-pointer hover:bg-gray-800">
@@ -133,8 +135,8 @@ function Home() {
                 high-quality solutions that exceed our clients' expectations.
               </p>
             </div>
-            <div className="flex flex-wrap justify-center gap-10">
-              <div className="w-80 bg-gray-100 p-5 rounded-lg shadow-md">
+            <div className="flex flex-wrap justify-center gap-10 ">
+              <div className="w-80 bg-gray-100 p-5 rounded-lg shadow-md hover:bg-green-500">
                 <h3 className="text-xl font-bold text-black mb-3">
                   Our Vision
                 </h3>
@@ -143,7 +145,7 @@ function Home() {
                   solutions in the industry.
                 </p>
               </div>
-              <div className="w-80 bg-gray-100 p-5 rounded-lg shadow-md">
+              <div className="w-80 bg-gray-100 p-5 rounded-lg shadow-md  hover:bg-green-500">
                 <h3 className="text-xl font-bold text-black mb-3">
                   Our Mission
                 </h3>
@@ -152,7 +154,7 @@ function Home() {
                   their goals and drive success.
                 </p>
               </div>
-              <div className="w-80 bg-gray-100 p-5 rounded-lg shadow-md">
+              <div className="w-80 bg-gray-100 p-5 rounded-lg shadow-md hover:bg-green-500">
                 <h3 className="text-xl font-bold text-black mb-3">
                   Our Values
                 </h3>
@@ -177,7 +179,7 @@ function Home() {
               </p>
             </div>
             <div className="flex flex-wrap justify-center gap-10">
-              <div className="w-80 bg-gray-100 p-5 rounded-lg shadow-md">
+              <div className="w-80 bg-gray-100 p-5 rounded-lg shadow-md  hover:bg-green-500">
                 <h3 className="text-xl font-bold text-black mb-3">
                   Web Development
                 </h3>
@@ -186,7 +188,7 @@ function Home() {
                   your business needs.
                 </p>
               </div>
-              <div className="w-80 bg-gray-100 p-5 rounded-lg shadow-md">
+              <div className="w-80 bg-gray-100 p-5 rounded-lg shadow-md  hover:bg-green-500">
                 <h3 className="text-xl font-bold text-black mb-3">
                   Mobile Development
                 </h3>
@@ -195,7 +197,7 @@ function Home() {
                   iOS and Android platforms.
                 </p>
               </div>
-              <div className="w-80 bg-gray-100 p-5 rounded-lg shadow-md">
+              <div className="w-80 bg-gray-100 p-5 rounded-lg shadow-md  hover:bg-green-500">
                 <h3 className="text-xl font-bold text-black mb-3">
                   UI/UX Design
                 </h3>
@@ -204,7 +206,7 @@ function Home() {
                   a seamless user experience.
                 </p>
               </div>
-              <div className="w-80 bg-gray-100 p-5 rounded-lg shadow-md">
+              <div className="w-80 bg-gray-100 p-5 rounded-lg shadow-md  hover:bg-green-500">
                 <h3 className="text-xl font-bold text-black mb-3">
                   Digital Marketing
                 </h3>
@@ -280,7 +282,7 @@ function Home() {
 
         <footer className="py-4 sm:items-center">
           <nav className="flex justify-between items-center gap-10">
-            <ul className="flex gap-5 text-black text-sm font-medium">
+            <ul className="flex gap-5 text-black text-sm font-medium md:text-base md:gap-10">
               <li>
                 <a href="#home" className="hover:text-green-500">
                   Home
