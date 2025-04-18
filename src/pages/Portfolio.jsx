a00078c92366da4bbdca9dba1d5a951c7ec6ce43
anthax589 <jaymartuba2@gmail.com>
2025-04-18 19:47:43 +0800

    Replace profile picture with SamplePic and update button styling

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -9,7 +9,7 @@ import {
 import WebDeveloperAnimation from "../assets/WebDeveloperAnimation.json";
 import ContactUs from "../assets/contactUs.json";
 import CodingAnimation from "../assets/CodingAnimation.json";
-import Me from "../assets/Me.jpg";
+import SamplePic from "../assets/SamplePic.jpeg";
 
 function Home() {
   const toggleNavbar = () => {
@@ -109,7 +109,7 @@ function Home() {
               <p className="text-2xl font-medium text-black mb-4 text-wrap py-2 md:text-4xl">
                 FrontEnd <span className="text-green-500">Developer</span>
               </p>
-              <button className="bg-black text-white text-sm py-2 px-4 rounded cursor-pointer hover:bg-gray-800 border-2 border-green-500 xs:p-2 xs:text-sm">
+              <button className="bg-black text-white text-sm py-2 px-4 rounded-lg cursor-pointer hover:bg-gray-800 border-2 border-green-500 xs:p-2 xs:text-sm">
                 Get Started
               </button>
             </div>
@@ -134,7 +134,7 @@ function Home() {
                 <div className="flex flex-col sm:flex-row mt-10 ">
                   <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8 ">
                     <div className="w-40 h-40 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400 ">
-                      <img src={Me} alt="myPic" />
+                      <img src={SamplePic} alt="myPic" />
                     </div>
                     <div className="flex flex-col items-center text-center justify-center">
                       <h2 className="font-medium title-font mt-4 text-gray-900 text-lg">
@@ -321,6 +321,7 @@ function Home() {
                 <button
                   className="bg-black text-white text-sm py-2 px-4 rounded focus:outline-none focus:shadow-outline hover:bg-gray-800"
                   type="submit"
+                  linkto="jaymartuba2@gmail.com"
                 >
                   Send
                 </button>
