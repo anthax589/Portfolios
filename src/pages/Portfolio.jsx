524cb79c5b7bf71b67d843641c24672cce7795a4
anthax589 <jaymartuba2@gmail.com>
2025-05-08 23:48:53 +0800

    Update the background and add some new style and content

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -10,6 +10,11 @@ import WebDeveloperAnimation from "../assets/WebDeveloperAnimation.json";
 import ContactUs from "../assets/contactUs.json";
 import CodingAnimation from "../assets/CodingAnimation.json";
 import SamplePic from "../assets/SamplePic.jpeg";
+import { FaArrowRightLong } from "react-icons/fa6";
+import { SiXdadevelopers } from "react-icons/si";
+import { GrVmMaintenance } from "react-icons/gr";
+import { GrOptimize } from "react-icons/gr";
+
 
 function Home() {
   const toggleNavbar = () => {
@@ -23,11 +28,11 @@ function Home() {
 
   return (
     <div lang="en" className="scroll-smooth">
-      <title>Marix</title>
+      <title>Portfolio</title>
 
-      <nav className="bg-white fixed top-0 left-0 w-full z-50 shadow-md ">
+      <nav className="bg-[#278783] fixed top-0 left-0 w-full z-50 shadow-md ">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <h1 className="text-green-500 sm:text-2xl">AnThrax</h1>
+          <h1 className="text-[#ffebd0] sm:text-2xl">AnThrax</h1>
           <button
             onClick={toggleNavbar}
             data-collapse-toggle="navbar-default"
@@ -38,7 +43,7 @@ function Home() {
           >
             <span className="sr-only">menu</span>
             <svg
-              className="w-5 h-5 text-black"
+              className="w-5 h-5 text-[#ffebd0]"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -54,11 +59,11 @@ function Home() {
             </svg>
           </button>
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-transparent items-center  md:gap-8 ">
+            <ul className="font-medium flex flex-col p-4 md:p-0  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-transparent items-center  md:gap-6 ">
               <li>
                 <a
                   href="#home"
-                  className="block items-center py-2 px-3 rounded md:bg-transparent md:text-green-500 md:p-0 dark:text-black hover:text-green-500"
+                  className="block items-center py-2 px-3 rounded md:bg-transparent md:text-green-500 md:p-0 dark:text-[#ffebd0] hover:text-green-500"
                 >
                   Home
                 </a>
@@ -66,7 +71,7 @@ function Home() {
               <li>
                 <a
                   href="#about"
-                  className="block py-2 px-3 text-gray-900 rounded  md:hover:bg-transparent md:border-0 hover:text-green-500 md:p-0 dark:text-black "
+                  className="block py-2 px-3 text-gray-900 rounded  md:hover:bg-transparent md:border-0 hover:text-green-500 md:p-0 dark:text-[#ffebd0]"
                 >
                   About
                 </a>
@@ -74,7 +79,7 @@ function Home() {
               <li>
                 <a
                   href="#services"
-                  className="block py-2 px-3 text-gray-900 rounded  md:border-0 hover:text-green-500 md:p-0 dark:text-black "
+                  className="block py-2 px-3 text-gray-900 rounded  md:border-0 hover:text-green-500 md:p-0 dark:text-[#ffebd0]"
                 >
                   Services
                 </a>
@@ -82,7 +87,7 @@ function Home() {
               <li>
                 <a
                   href="#contact"
-                  className="block py-2 px-3 text-gray-900 rounded md:hover:bg-transparent md:border-0 hover:text-green-500 md:p-0 dark:text-black md:dark:hover:text-green-500 "
+                  className="block py-2 px-3 text-gray-900 rounded md:hover:bg-transparent md:border-0 hover:text-green-500 md:p-0 dark:text-[#ffebd0] md:dark:hover:text-green-500 "
                 >
                   Contact
                 </a>
@@ -92,32 +97,32 @@ function Home() {
         </div>
       </nav>
 
-      <main className="px-5 md:px-10 md:text-1xl lg:px-40 xl:px-60 bg-gray-300 dark:bg-gray-300">
+      <main className="px-5 md:px-10 md:text-1xl lg:px-40 xl:px-60 bg-[#DFE8E6] dark:bg-[#ffebd0]">
         <section className="min-h-screen" id="home">
           <div className="flex flex-col  items-center justify-center s: py-14 xs:pt-30 md:gap-60  md:flex-row min-h-screen md:pb-28 lg:flex-row ">
             <div className="md:p-5 md:items-center md:justify-center ">
-              <h1 className="text-2xl font-medium text-black md:text-4xl">
+              <h1 className="text-2xl font-medium text-[#278783] md:text-4xl">
                 Hi! everyone,{" "}
               </h1>
-              <h1 className=" text-2xl font-medium text-black xs:text-2xl md:text-4xl lg:text-1xl">
+              <h1 className=" text-2xl font-medium text-[#278783] xs:text-2xl md:text-4xl lg:text-1xl">
                 I'm
-                <span className=" text-4xl md:text-6xl lg:text-5xl">
+                <span className=" text-4xl md:text-6xl lg:text-5xl ">
                   {" "}
                   Jaymar Tuba
                 </span>
               </h1>
-              <p className="text-2xl font-medium text-black mb-4 text-wrap py-2 md:text-4xl">
-                Web <span className="text-green-500">Developer</span>
+              <p className="text-2xl font-medium text-[#278783] mb-4 text-wrap py-2 md:text-4xl">
+                Web <span className="text-[#278783]">Developer</span>
               </p>
-              <button className="bg-black text-white text-sm py-2 px-4 rounded-lg cursor-pointer hover:bg-gray-800 border-2 border-green-500 xs:p-2 xs:text-sm">
+              {/* <button className="bg-black text-white text-sm py-2 px-4 rounded-lg cursor-pointer hover:bg-gray-800 border-2 border-green-500 xs:p-2 xs:text-sm">
                 Get Started
-              </button>
+              </button> */}
             </div>
             <div className="relative flex items-center justify-center xs:relative top-10 xs:w-45 h-45 md:w-80 md:h-80 md:items-end md:justify-end lg:w-95 lg:h-90 lg:items-end lg:justify-end">
               <Lottie animationData={CodingAnimation} />
             </div>
           </div>
-          <div className=" flex justify-center gap-16 py-10 text-green-500 s:text-2xl xs:gap-10 xs:text-xl md:relative bottom-20 md:text-2xl ">
+          <div className=" flex justify-center gap-16 py-10 text-[#278783] s:text-2xl xs:gap-10 xs:text-xl md:relative bottom-20 md:text-2xl ">
             <AiFillTwitterCircle className="hover:text-gray-800" />
             <AiFillGithub className="hover:text-gray-800" />
             <AiFillFacebook className="hover:text-gray-800" />
@@ -128,7 +133,7 @@ function Home() {
         <hr className="h-px bg-white border-0 dark:bg-black xs: relative top-0 " />
 
         <section id="about" className="min-h-screen">
-          <section className="text-gray-600 body-font">
+          <section className="text-[#278783] body-font">
             <div className="container px-5 py-24 mx-auto flex flex-cols xs:py-5 s:py-5">
               <div className="lg:w-4/6 mx-auto">
                 <div className="flex flex-col sm:flex-row mt-10 ">
@@ -137,10 +142,10 @@ function Home() {
                       <img src={SamplePic} alt="myPic" />
                     </div>
                     <div className="flex flex-col items-center text-center justify-center">
-                      <h2 className="font-medium title-font mt-4 text-gray-900 text-lg">
+                      <h2 className="font-medium title-font mt-4 text-[#278783] text-lg">
                         Jaymar Abao Tuba
                       </h2>
-                      <div className="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4 cursor-pointer"></div>
+                      <div className="w-12 h-1 bg-[#278783] last:rounded mt-2 mb-4 cursor-pointer"></div>
                       <p className="text-base">Web Developer</p>
                     </div>
                   </div>
@@ -158,20 +163,10 @@ function Home() {
                     </p>
                     <a
                       href="#contact"
-                      className="text-indigo-500 inline-flex items-center pl-10 xs:p-0"
+                      className="text-[#278783] inline-flex items-center   xs:py-2 xs:px-4 xs:text-sm md:py-2 md:px-6 md:text-lg lg:py-3 lg:px-6 lg:text-xl border-2 border-[#278783] rounded-lg hover:bg-[#278783] hover:text-white cursor-pointer"
                     >
-                      Learn More
-                      <svg
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        className="w-4 h-4 ml-2"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M5 12h14M12 5l7 7-7 7"></path>
-                      </svg>
+                      Projects
+                      <FaArrowRightLong className="ml-2 mt-1" />
                     </a>
                   </div>
                 </div>
@@ -187,79 +182,55 @@ function Home() {
             <div className=" pt-20 lg:w-1/2  mb-10  lg:mb-0 rounded-lg overflow-hidden xs:pt-10 s:pt-0">
               <Lottie animationData={WebDeveloperAnimation} />
             </div>
-            <div className="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center xs:pb-0 s:pb-10">
-              <div className="flex flex-col mb-10 lg:items-start items-center">
+            <div className="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center xs:pb-0 s:pb-10 ">
+              <div className="flex flex-col mb-10 lg:items-start items-center border-2 border-[#278783] rounded-lg p-5">
                 <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    className="w-6 h-6"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-                  </svg>
+                  <SiXdadevelopers className="w-6 h-6 text-[#278783]" />
                 </div>
                 <div className="flex-grow ">
-                  <h2 className="text-gray-900 text-lg title-font font-medium mb-3">
-                    Shooting Stars
+                  <h2 className="text-[#278783] text-lg title-font font-medium mb-3">
+                    E-commerce Development
                   </h2>
-                  <p className="leading-relaxed text-base">
-                    Blue bottle crucifix vinyl post-ironic four dollar toast
-                    vegan taxidermy. Gastropub indxgo juice poutine.
+                  <p className="leading-relaxed text-base text-[#278783] ">
+                    <ul>
+                      <li>Online store setup</li>
+                      <li>Payment gateway integration</li>
+                      <li>Shopping cart functionality</li>
+                    </ul>
                   </p>
                 </div>
               </div>
-              <div className="flex flex-col mb-10 lg:items-start items-center">
+              <div className="flex flex-col mb-10 lg:items-start items-center border-2 border-[#278783] rounded-lg p-5">
                 <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    className="w-6 h-6"
-                    viewBox="0 0 24 24"
-                  >
-                    <circle cx="6" cy="6" r="3"></circle>
-                    <circle cx="6" cy="18" r="3"></circle>
-                    <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
-                  </svg>
+                  <GrVmMaintenance className="w-6 h-6 text-[#278783]" />
                 </div>
                 <div className="flex-grow">
-                  <h2 className="text-gray-900 text-lg title-font font-medium mb-3">
-                    The Catalyzer
+                  <h2 className="text-[#278783] text-lg title-font font-medium mb-3">
+                    Website Maintenance & Support
                   </h2>
-                  <p className="leading-relaxed text-base">
-                    Blue bottle crucifix vinyl post-ironic four dollar toast
-                    vegan taxidermy. Gastropub indxgo juice poutine.
+                  <p className="leading-relaxed text-base text-[#278783] ">
+                    <ul>
+                      <li>Regular updates and backups</li>
+                      <li>Security monitoring and fixes</li>
+                      <li>Performance optimization</li>
+                    </ul>
                   </p>
                 </div>
               </div>
-              <div className="flex flex-col mb-10 lg:items-start items-center">
+              <div className="flex flex-col mb-10 lg:items-start items-center border-2 border-[#278783] rounded-lg p-5">
                 <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    className="w-6 h-6"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                    <circle cx="12" cy="7" r="4"></circle>
-                  </svg>
+                  <GrOptimize className="w-6 h-6 text-[#278783]" />
                 </div>
-                <div className="flex-grow">
-                  <h2 className="text-gray-900 text-lg title-font font-medium mb-3">
-                    Neptune
+                <div className="flex-grow ">
+                  <h2 className="text-[#278783] text-lg title-font font-medium mb-3">
+                    Web Performance Optimization
                   </h2>
-                  <p className="leading-relaxed text-base">
-                    Blue bottle crucifix vinyl post-ironic four dollar toast
-                    vegan taxidermy. Gastropub indxgo juice poutine.
+                  <p className="leading-relaxed text-base text-[#278783]">
+                    <ul>
+                      <li>Page speed optimization</li>
+                      <li>Caching implementation</li>
+                      <li>Code optimization</li>
+                    </ul>
                   </p>
                 </div>
               </div>
@@ -273,11 +244,11 @@ function Home() {
           <div className=" container xs:pt-10 md:py-56 flex md:flex-row items-center justify-center xs:flex flex-col-reverse ">
             <form
               action="#"
-              className="max-w-lg mx-auto bg-gray-100 p-8 rounded-lg shadow-md md:w-1/2"
+              className="max-w-lg mx-auto border-2 border-[#278783] rounded-lg p-5 shadow-md md:w-1/2"
             >
               <div className="mb-4">
                 <label
-                  className="block text-gray-700 text-sm font-bold mb-2"
+                  className="block text-[#278783] text-sm font-bold mb-2"
                   htmlFor="name"
                 >
                   Name
@@ -291,7 +262,7 @@ function Home() {
               </div>
               <div className="mb-4">
                 <label
-                  className="block text-gray-700 text-sm font-bold mb-2"
+                  className="block text-[#278783]  text-sm font-bold mb-2"
                   htmlFor="email"
                 >
                   Email
@@ -305,7 +276,7 @@ function Home() {
               </div>
               <div className="mb-4">
                 <label
-                  className="block text-gray-700 text-sm font-bold mb-2"
+                  className="block text-[#278783]  text-sm font-bold mb-2"
                   htmlFor="message"
                 >
                   Message
@@ -319,7 +290,7 @@ function Home() {
               </div>
               <div className="flex items-center justify-between text-center ">
                 <button
-                  className="bg-black text-white text-sm py-2 px-4 rounded focus:outline-none focus:shadow-outline hover:bg-gray-800"
+                  className=" border-2 border-[#278783] bg-[#278783] text-[#ffebd0] text-sm py-2 px-4 rounded focus:outline-none focus:shadow-outline hover:bg-gray-800"
                   type="submit"
                   linkto="jaymartuba2@gmail.com"
                 >
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
@@ -30,5 +31,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
